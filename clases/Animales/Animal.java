63e705345d109ef4a99f110db80ea550c72a8243
pirer-al <zs23017378@estudiantes.uv.mx>
2025-09-23 06:42:58 -0600

    Create Animal.java

---
+++ clases/Animales/Animal.java
@@ -0,0 +1,9 @@
+package Animales;
+
+public abstract class Animal {
+    public abstract void hacerSonido();
+
+    public void dormir() {
+        System.out.println("El animal está durmiendo");
+    }
+}
